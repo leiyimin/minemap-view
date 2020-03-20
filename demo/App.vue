62db3e4c633f241eb83e65ceec3e0cb56964f8b0
helihuo <leizi840228>
2020-03-20 23:57:45 +0800

    引入prettier,规范代码

--- demo/App.vue
+++ demo/App.vue
@@ -2,22 +2,25 @@
   <div id="app">
     <MapView v-bind="mapConfig" @completed="mapLoad"></MapView>
     <div class="left">
-      <MapView v-for="(map,i) in mapList" :key="i"
-               v-bind="map"
-               @completed="mapLoad"
-               :height="260"></MapView>
+      <MapView
+        v-for="(map, i) in mapList"
+        :key="i"
+        v-bind="map"
+        @completed="mapLoad"
+        :height="260"
+      ></MapView>
     </div>
   </div>
 </template>
 
 <script>
-
-  export default {
-    name: "App",
-    data() {
-      let domainUrl = 'https://map.xianjiaojing.com';
-      return {
-        mapList: [{
+export default {
+  name: 'App',
+  data() {
+    let domainUrl = 'https://map.xianjiaojing.com';
+    return {
+      mapList: [
+        {
           accessToken: 'e919a6f32ce242f5aec22652d9dc1fdb',
           solution: '4287',
           options: {
@@ -27,8 +30,9 @@
             zoom: 9,
             minZoom: 3,
             maxZoom: 17,
-          }
-        }, {
+          },
+        },
+        {
           accessToken: 'e919a6f32ce242f5aec22652d9dc1fdb',
           solution: 7185,
           options: {
@@ -37,54 +41,54 @@
             center: [116.1866179, 39.992559],
             zoom: 9,
             minZoom: 3,
-            maxZoom: 17
-          }
-        }],
-        mapConfig: {
-          accessToken: '18f589098bf14f5bac790331c60a37ae',
-          solution: '3492',
-          options: {
-            container: 'map',
-            style: domainUrl + '/service/solu/style/id/3492',
-            center: [116.1866179, 39.992559],
-            zoom: 9,
-            minZoom: 3,
             maxZoom: 17,
-            logoControl: true
           },
-          urls: {
-            domainUrl: domainUrl,
-            dataDomainUrl: domainUrl,
-            serviceUrl: domainUrl + '/service',
-          }
-        }
-      }
-    },
-    mounted() {
-    },
-    methods: {
-      mapLoad() {
-      }
-    }
-  }
+        },
+      ],
+      mapConfig: {
+        accessToken: '18f589098bf14f5bac790331c60a37ae',
+        solution: '3492',
+        options: {
+          container: 'map',
+          style: domainUrl + '/service/solu/style/id/3492',
+          center: [116.1866179, 39.992559],
+          zoom: 9,
+          minZoom: 3,
+          maxZoom: 17,
+          logoControl: true,
+        },
+        urls: {
+          domainUrl: domainUrl,
+          dataDomainUrl: domainUrl,
+          serviceUrl: domainUrl + '/service',
+        },
+      },
+    };
+  },
+  mounted() {},
+  methods: {
+    mapLoad() {},
+  },
+};
 </script>
 
 <style scoped>
-  html, body {
-    margin: 0;
-    padding: 0;
-  }
+html,
+body {
+  margin: 0;
+  padding: 0;
+}
 
-  #app {
-    display: flex;
-    top: 0;
-    height: 100vh;
-  }
+#app {
+  display: flex;
+  top: 0;
+  height: 100vh;
+}
 
-  .left {
-    position: absolute;
-    top: 0;
-    height: 100vh;
-    width: 300px;
-  }
+.left {
+  position: absolute;
+  top: 0;
+  height: 100vh;
+  width: 300px;
+}
 </style>
--- demo/main.js
+++ demo/main.js
@@ -1,9 +1,9 @@
-import Vue from 'vue'
-import App from './App.vue'
-import MapView from '../index'
+import Vue from 'vue';
+import App from './App.vue';
+import MapView from '../index';
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
 Vue.use(MapView);
 new Vue({
   render: h => h(App),
-}).$mount('#app')
+}).$mount('#app');
